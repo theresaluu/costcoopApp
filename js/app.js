4141e949a395a5c42c8e116de4a71b1c8935ca27
Theresa Luu <theresa.luu@gmail.com>
2013-08-29 19:03:20 -0700

    things are posting to db

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
+// function Controller($scope) {
+//   $scope.master= {};
+ 
+//   $scope.update = function(user) {
+//     $scope.master= angular.copy(user);
+//   };
+ 
+//   $scope.reset = function() {
+//     $scope.user = angular.copy($scope.master);
+//   };
+ 
+//   $scope.reset();
+// }
+
+
+
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+angular.module('costcoop', 
+	[
+	'firebase',
+	'costcoop.controllers'
+	]
+	);
+ 
